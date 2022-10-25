3243b4d85c672bbd4fcf034b21c14083f1573cc6
BottledBananas <rubentiehuis@msn.com>
2022-10-25 20:20:56 +0200

    added more pseudocode

--- app.js
+++ app.js
@@ -3,13 +3,12 @@
     const fields = []
     const gameBoard = document.querySelector("#gameBoard")
     let numField = 0
-
-
+    let numTurns = 0
 
     //function creates an array with 9 entries, all filled with "x"
     function board() {
         for (let i = 0; i < 9; i++) {
-            fields[i] = "x";
+            fields[i] = "";
             console.log("hello");
         } 
         
@@ -21,12 +20,76 @@
             let newField = document.createElement("div");
             newField.textContent = field;
             newField.classList.add(`field${numField}`)
-            newField.addEventListener('click', function test() {console.log('helloooo')})
+            newField.addEventListener('click', function test() {newField.innerText = "x"})
             gameBoard.append(newField);
         }
-        console.log(fields)
     }
     board()
+
+    function init() {
+        //ask for name 
+
+            //if amount of players === 0 put name in label .playerOne
+            //factPlayers(name)
+            //playerCounter++
+            //ask for name
+
+            //if amount of players === 1 put name in label .playerTwo
+            //factPlayers(name)
+            //close names input
+            //board()
+            //begin()
+    }
+
+    function factPlayers(name) {
+        //create Player object with
+        //  -player name
+        //  -assigned character (x/o)
+        //  -score
+    }
+
+    function begin() {
+        //resets board (let board() empty playingfield)
+        //numTurns = 0
+        //randomly choose a player to get the first turn
+        //turn()
+    }
+
+    function turn() {
+        //selected player may a field
+        //clicked field content turns into player's icon; X or O
+        //check if winning conditions are met and if so run win()
+        //numTurns++
+        //other player gets selected
+        //turn()
+    }
+
+    function win() {
+        //check if win conditions are met
+        //if so, winner()
+        //else, turn()
+    }
+
+    function winner() {
+        //does something cool when winning conditions are met
+        //display "Player X wins" for instance
+        //winning player's score++
+        //begin()
+    }
+
+    function scoreReset() {
+        //both players' scores = 0
+        //begin()
+    }
+
+    function fullReset() {
+        //reload the page
+        //OR
+        //players' scores = 0
+        //numTurns = 0
+        //init()
+    }
+
 })()
 
 //board with 9 fields
